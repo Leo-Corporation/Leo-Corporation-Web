bf8477e611320f534f7d161572325b962eab9ab7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-17 14:14:30 +0200

    Animations only play if the element is visible

---
+++ docs/scripts/animations.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @param {string} elementId The element to "track".
+ * @param {string} classToApply The class to apply to the element.
+ */
+function animateOnView(elementId, classToApply) {
+    const observer = new IntersectionObserver(entries => {
+        // Loop over the entries
+        entries.forEach(entry => {
+            // If the element is visible
+            if (entry.isIntersecting) {
+                // Add the animation class
+                entry.target.classList.add(classToApply);
+            }
+        });
+    });
+    observer.observe(document.getElementById(elementId));
+}
+
+animateOnView("popular-item-1", "slideInUp");
+animateOnView("popular-item-2", "slideInUp");
+animateOnView("popular-item-3", "slideInUp");
+animateOnView("new-soft-item-1", "slideInUp");
+animateOnView("new-soft-item-2", "slideInUp");
+animateOnView("new-soft-item-3", "slideInUp");
+animateOnView("opensource-item-1", "slideInUp");
+animateOnView("opensource-item-2", "slideInUp");
+animateOnView("opensource-item-3", "slideInUp");
