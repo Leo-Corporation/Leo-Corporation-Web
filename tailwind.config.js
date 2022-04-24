1459e3ea8abc8e8f68629a2c3f0764d6a420a601
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-04-24 18:10:59 +0200

    Added a colorful animation in "Home"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'move-bg': {
+          to: {
+            backgroundPosition: '400% 0',
+          },
+        },
+      },
+      animation: {
+        'move-bg': 'move-bg 8s infinite linear',
+      },
+    },
   },
   plugins: [],
 }
