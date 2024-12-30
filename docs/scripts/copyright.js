37e6b83c591a4d0d057fa805df5877c2d5d04242
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-12-30 10:51:00 +0100

    Added new footer

--- docs/scripts/copyright.js
+++ docs/scripts/copyright.js
@@ -1,2 +1,2 @@
 document.getElementById("copyright").innerText =
-  "© " + new Date().getFullYear() + " Peyronnet Group and Léo Corporation";
+  "© " + new Date().getFullYear() + " GRP and Léo Corporation";
